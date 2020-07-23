84c3e60f08c0a824185cebb30483b890e7261d20
diwesh2504 <66382963+diwesh2504@users.noreply.github.com>
2020-07-23 21:40:20 +0530

    Updates

---
+++ precache-manifest.6de697d8eddb71ca7257fdeef6ebb0d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c831b6ab7b3b4b4a2dfe63a139f9431",
+    "url": "/movie-imdb/index.html"
+  },
+  {
+    "revision": "5780ac2f65031c632beb",
+    "url": "/movie-imdb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69a4e4316f5bc460f644",
+    "url": "/movie-imdb/static/js/2.445f3282.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-imdb/static/js/2.445f3282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5780ac2f65031c632beb",
+    "url": "/movie-imdb/static/js/main.cdf1ee90.chunk.js"
+  },
+  {
+    "revision": "3f625c11c8bb8edb4f1c",
+    "url": "/movie-imdb/static/js/runtime-main.b2f1bd33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-imdb/static/media/logo.5d5d9eef.svg"
+  }
+]);
